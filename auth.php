5c4e362009832f056715eecc5a1a4820fdabba07
kundan sow <113882686+kndnsow@users.noreply.github.com>
2023-10-05 17:23:41 +0530

    update 1.0.1

--- auth.php
+++ auth.php
@@ -7,43 +7,42 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 defined('MOODLE_INTERNAL') || die();
-require_once($CFG->libdir.'/authlib.php');
+require_once $CFG->libdir . '/authlib.php';
 class auth_plugin_moowoodle_moodle_connector extends auth_plugin_base {
 
 	public function __construct() {
-			$this->authtype = 'moowoodle_moodle_connector';
-			$this->config = get_config('auth_moowoodle_moodle_connector');
+		$this->authtype = 'moowoodle_moodle_connector';
+		$this->config = get_config('auth_moowoodle_moodle_connector');
 	}
 
-	public function user_login ($username, $password = null) {
+	public function user_login($username, $password = null) {
 		global $CFG, $DB;
-		if ($password == null || $password == '') { return false; }	
-		if ($user = $DB->get_record('user', array('username'=>$username, 'mnethostid'=>$CFG->mnet_localhost_id))) {
-						return true;
+		if ($password == null || $password == '') {return false;}
+		if ($user = $DB->get_record('user', array('username' => $username, 'mnethostid' => $CFG->mnet_localhost_id))) {
+			return true;
 		}
 		return false;
 	}
 
- 	public function can_reset_password()
-  {
-      return false;
-  }
+	public function can_reset_password() {
+		return false;
+	}
 
 	public function can_change_password() {
 		return false;
-  }
+	}
 
-  public function change_password_url() {
-   	return;
-  }
+	public function change_password_url() {
+		return;
+	}
 
-  public function is_internal() {
+	public function is_internal() {
 		return false;
-  }
+	}
 
-  public function prevent_local_passwords() {
-    return false;
-  }
+	public function prevent_local_passwords() {
+		return false;
+	}
 
 }
 ?>
--- settings.php
+++ settings.php
@@ -10,26 +10,22 @@ defined('MOODLE_INTERNAL') || die;
 $yesno = array(get_string('yes'), get_string('no'));
 // Adding tab setting for the stripepaymentpro.
 $settings = new auth_moowoodle_moodle_connector_admin_settingspage_tabs(
-    'auth_moowoodle_moodle_connector',
-    get_string('pluginname', 'auth_moowoodle_moodle_connector')
+	'auth_moowoodle_moodle_connector',
+	get_string('pluginname', 'auth_moowoodle_moodle_connector')
 );
 
 //General Settings tab
 $page = new admin_settingpage('auth_moowoodle_moodle_connector_general_settings', new lang_string('moowoodle_settings', 'auth_moowoodle_moodle_connector'));
 
 $page->add(new admin_setting_heading('auth_moowoodle_moodle_connector/pluginname', '',
-							new lang_string('auth_moowoodledescription', 'auth_moowoodle_moodle_connector')));
+	new lang_string('auth_moowoodledescription', 'auth_moowoodle_moodle_connector')));
 $page->add(new admin_setting_configtext('auth_moowoodle_moodle_connector/encryptkey', get_string('key', 'auth_moowoodle_moodle_connector'),
-						get_string('message','auth_moowoodle_moodle_connector','auth'),'', PARAM_RAW));
+	get_string('message', 'auth_moowoodle_moodle_connector', 'auth'), '', PARAM_RAW));
 $page->add(new admin_setting_configtext('auth_moowoodle_moodle_connector/wpsiteurl', get_string('wpsiteurl', 'auth_moowoodle_moodle_connector'),
-						get_string('message2','auth_moowoodle_moodle_connector','auth'),'', PARAM_RAW));
+	get_string('message2', 'auth_moowoodle_moodle_connector', 'auth'), '', PARAM_RAW));
 $page->add(new admin_setting_configtext('auth_moowoodle_moodle_connector/timelimit', get_string('timelimit', 'auth_moowoodle_moodle_connector'),
-						get_string('message3','auth_moowoodle_moodle_connector','auth'),'5', PARAM_INT));
-
-
-
+	get_string('message3', 'auth_moowoodle_moodle_connector', 'auth'), '5', PARAM_INT));
 
 $settings->add($page);
-
 
 ?>
